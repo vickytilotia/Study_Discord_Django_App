37614506759f7bad11b75df9264fbd8dca3bc676
Vivek Kumar <32337899+vickytilotia@users.noreply.github.com>
2022-02-09 12:34:51 +0530

    favicon and allowed hosts 2

--- study_discord/settings.py
+++ study_discord/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY'] # for heroku
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
