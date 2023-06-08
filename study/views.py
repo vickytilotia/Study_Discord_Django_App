8d2f8164fb0148bf44c2d00498dbacea336a3748
Vivek Kumar <32337899+vickytilotia@users.noreply.github.com>
2023-06-08 16:39:47 +0530

    fixed recent activities

--- study/views.py
+++ study/views.py
@@ -87,7 +87,7 @@ def home(request):
 
     topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
-    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))[0:4]
 
     context = {'rooms':rooms, 
     'topics':topics, 
